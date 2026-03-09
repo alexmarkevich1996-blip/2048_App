20d5545b529d2bc1f8b970cd69843f9d79abb6f6
aleksandr <aleksandr@A9B3>
2026-03-09 12:07:02 +0300

    feat(ui): add label to display 2048 board

---
+++ 2048_WinForms_App/MainForm.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace _2048_WinForms_App
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label = new Label();
+            boardLabel = new Label();
+            emtyCellLabel1 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            SuspendLayout();
+            // 
+            // label
+            // 
+            label.BackColor = Color.RosyBrown;
+            label.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label.Location = new Point(763, 240);
+            label.Name = "label";
+            label.Size = new Size(56, 53);
+            label.TabIndex = 0;
+            label.Text = "2048";
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // boardLabel
+            // 
+            boardLabel.BackColor = Color.BurlyWood;
+            boardLabel.Location = new Point(318, 177);
+            boardLabel.Name = "boardLabel";
+            boardLabel.Size = new Size(284, 277);
+            boardLabel.TabIndex = 1;
+            // 
+            // emtyCellLabel1
+            // 
+            emtyCellLabel1.BackColor = Color.AntiqueWhite;
+            emtyCellLabel1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            emtyCellLabel1.Location = new Point(341, 196);
+            emtyCellLabel1.Name = "emtyCellLabel1";
+            emtyCellLabel1.Size = new Size(56, 53);
+            emtyCellLabel1.TabIndex = 2;
+            emtyCellLabel1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.AntiqueWhite;
+            label1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(403, 196);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 53);
+            label1.TabIndex = 3;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.AntiqueWhite;
+            label2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(465, 196);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 53);
+            label2.TabIndex = 4;
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.AntiqueWhite;
+            label3.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(527, 196);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 53);
+            label3.TabIndex = 5;
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.AntiqueWhite;
+            label4.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(341, 258);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 53);
+            label4.TabIndex = 6;
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.BackColor = Color.AntiqueWhite;
+            label5.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(403, 258);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 53);
+            label5.TabIndex = 7;
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.AntiqueWhite;
+            label6.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(465, 320);
+            label6.Name = "label6";
+            label6.Size = new Size(56, 53);
+            label6.TabIndex = 8;
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            label7.BackColor = Color.AntiqueWhite;
+            label7.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(465, 258);
+            label7.Name = "label7";
+            label7.Size = new Size(56, 53);
+            label7.TabIndex = 9;
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            label8.BackColor = Color.AntiqueWhite;
+            label8.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(341, 320);
+            label8.Name = "label8";
+            label8.Size = new Size(56, 53);
+            label8.TabIndex = 10;
+            label8.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            label9.BackColor = Color.AntiqueWhite;
+            label9.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(403, 320);
+            label9.Name = "label9";
+            label9.Size = new Size(56, 53);
+            label9.TabIndex = 11;
+            label9.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            label10.BackColor = Color.AntiqueWhite;
+            label10.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(527, 258);
+            label10.Name = "label10";
+            label10.Size = new Size(56, 53);
+            label10.TabIndex = 12;
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            label11.BackColor = Color.AntiqueWhite;
+            label11.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(527, 320);
+            label11.Name = "label11";
+            label11.Size = new Size(56, 53);
+            label11.TabIndex = 13;
+            label11.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            label12.BackColor = Color.AntiqueWhite;
+            label12.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.Location = new Point(341, 382);
+            label12.Name = "label12";
+            label12.Size = new Size(56, 53);
+            label12.TabIndex = 14;
+            label12.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            label13.BackColor = Color.AntiqueWhite;
+            label13.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.Location = new Point(403, 382);
+            label13.Name = "label13";
+            label13.Size = new Size(56, 53);
+            label13.TabIndex = 15;
+            label13.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            label14.BackColor = Color.AntiqueWhite;
+            label14.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label14.Location = new Point(465, 382);
+            label14.Name = "label14";
+            label14.Size = new Size(56, 53);
+            label14.TabIndex = 16;
+            label14.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label15
+            // 
+            label15.BackColor = Color.AntiqueWhite;
+            label15.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label15.Location = new Point(527, 382);
+            label15.Name = "label15";
+            label15.Size = new Size(56, 53);
+            label15.TabIndex = 17;
+            label15.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1048, 701);
+            Controls.Add(label15);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(emtyCellLabel1);
+            Controls.Add(boardLabel);
+            Controls.Add(label);
+            Name = "MainForm";
+            Text = "MainForm";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label;
+        private Label boardLabel;
+        private Label emtyCellLabel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Label label15;
+    }
+}
